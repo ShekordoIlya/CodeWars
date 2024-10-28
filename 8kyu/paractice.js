ee2303b5658fece63be1c5b76e8d233794200102
Ilya Shekordo <27phantom03@list.ru>
2024-10-28 22:17:48 +0300

    feat(practice): sum of numbers was done

--- 8kyu/paractice.js
+++ 8kyu/paractice.js
@@ -4,5 +4,28 @@ function solution(str) {
 
   return arr.reverse().join("");
 }
+//-----------------------------------
 
-console.log(solution("dlrow"));
+//Task 2
+function getSum(a, b) {
+  let sum = 0;
+
+  if (a < b) {
+    for (let i = a; i <= b; i++) {
+      sum += i;
+    }
+    return sum;
+  }
+  if (a > b) {
+    for (let j = a; j >= b; j--) {
+      sum += j;
+    }
+    return sum;
+  }
+  if (a == b) {
+    return a;
+  }
+}
+//-----------------------------------
+
+//Task 3
